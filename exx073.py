1da9ca4549ee93adbb51e6ee1c6f6f9d474490c7
alves-Moises <moisesingo_ra@hotmail.com>
2021-05-08 23:46:27 -0300

    exx 072 73

---
+++ exx073.py
@@ -0,0 +1,5 @@
+numbers = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 
+        'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte']
+
+for item in range(0, len(numbers)):
+    print(numbers[item])
